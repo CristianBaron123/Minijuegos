da225c1751e942a5eea4ce9f961b1924c875f2fa
CristianBaron123 <ozoratsubasa552@gmail.com>
2026-05-09 18:55:02 -0500

    move auth from moderator to perma banned list

--- bot.js
+++ bot.js
@@ -22,7 +22,6 @@ const OWNER_AUTH = "JHcYct4vfesGbi6tGaauh08AxSwWnZq3QCm4rnzn2GE";
 // CONFIGURACIÓN DE MODERADORES
 // ============================================
 const MODERATOR_AUTHS = [
-    "TSR_VIyn25T3PGuLRDLttjqS_2QXtLZVBCY7XkmTYpc",
     "4BTZsdo-dxI9p_wBCBtBpZHvxo91eqYQHUWGuWOkzqk",
     "py-h0135hpGNwKNn_I7n00W04x4tc6k2THJv1-Xj2ik",
 ];
